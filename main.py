3a86ccc7f6d6cce34a3869add7a6701b451aeff2
Kusa52 <kusa3238@gmail.com>
2025-05-07 21:33:28 -0400

    redo functions in main and stats

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print(f"{word_count()} words found in the document")
+from stats import get_word_count
 
-from stats import word_count
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    word_count = get_word_count(book_text)
+    print(f"{word_count} words found in the document")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
--- stats.py
+++ stats.py
@@ -1,10 +1,5 @@
-def get_book_text(path):
-    book_text = ""
-    with open(path) as f:
-        book_text = f.read()
-    return book_text
+def get_word_count(book_text):
+    return len(book_text.split())
 
-def word_count():
-    book_text = get_book_text("books/frankenstein.txt")
-    num_words = len(book_text.split())
-    return num_words
+def character_count():
+    pass
